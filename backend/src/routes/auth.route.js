a63d2c1638fdd0c91b8c82f5bc33a4b5954cb7a6
Aniket Dey <aniketdey2004@gmail.com>
2025-12-11 00:50:25 +0530

    Authentication middleware added, update profile middleware added,cloudinary image upload feature added"

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router=express.Router();
-import {signup, login, logout} from "../controllers/auth.controller.js";
+import {signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 router.post("/signup",signup);
 
@@ -8,4 +9,7 @@ router.post("/login",login);
 
 router.post("/logout",logout);
 
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req,res)=>res.status(200).json(req.user));
 export default router;
