7f538437f00ca765f9568d48cdb1eeb6c04a2263
Aniket Dey <aniketdey2004@gmail.com>
2025-12-13 08:51:58 +0530

    login page created and logout functionality integrated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore= create((set)=>({
    authUser:null,
    isCheckingAuth: true,
    isSigningUp:false,
+   isLoggingIn:false,
 
    checkAuth: async ()=>{
         try{
@@ -31,5 +32,29 @@ export const useAuthStore= create((set)=>({
         }finally{
             set({isSigningUp:false});
         }
+   },
+   login: async(data)=>{
+        set({isLoggingIn:true});
+        try{
+            const res=await axiosInstance.post("/auth/login",data);
+            set({authUser: res.data});
+            toast.success("Logged in Successfully")
+        }catch(error){
+            console.log("Error in Signing In", error);
+            toast.error(error.response.data.message);
+        }finally{
+            set({isLoggingIn:false});
+        }
+   },
+   logout:async()=>{
+     try{
+        await axiosInstance.post("/auth/logout");
+        set({authUser:null});  
+        toast.success("Logged out successfully");
+     }  
+     catch(error){
+        console.log("Error in logging out",error);
+        toast.error("Error in logging out");
+     }
    }
 }));
